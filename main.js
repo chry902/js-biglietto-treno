6208f782f1154a4b32e438f7b4e23d6270ee7d88
chry902 <tchry902@gmail.com>
2021-10-11 16:13:15 +0200

    inizio esercizio

---
+++ main.js
@@ -0,0 +1,7 @@
+//chiedere all'utente il numero di chilometri da percorrere
+
+const km_da_percorrere = prompt("km da percorrere");
+
+// età passeggero
+const eta_passeggero = prompt("età passeggero");
+
