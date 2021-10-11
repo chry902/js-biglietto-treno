17133f458181fbde18ad8b6b81a2652e6d4253be
chry902 <tchry902@gmail.com>
2021-10-11 16:35:44 +0200

    creando le const sconti

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const km_da_percorrere = prompt("km da percorrere");
 // età passeggero
 const eta_passeggero = prompt("età passeggero");
 
+//sconti
+
+const sconto_anziano = (km_da_percorrere * 0.21 - 40 % )
+const sconto_giovane = (km_da_percorrere * 0.21 - 20 %)
